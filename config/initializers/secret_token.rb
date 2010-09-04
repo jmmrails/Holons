1d41afa8188b0b7b5b6183d8cf3e96cd08267299
john.mullaly <john.mullaly@gmail.com>
2010-09-04 15:51:07 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Holons::Application.config.secret_token = 'c4f02f0bf5bcce433db7a51ae4a8d426854bf0389c3033422d7bffa973ecb9faa7c86a5242687f2bba1c7a01dc374e62b6c03514c08b26c4cce045ad0cf08abb'
